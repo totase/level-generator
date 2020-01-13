be6a872d3a3846c677fd5805c7f37d5d5772d2a0
Tommy Sevaldsen <tommysevaldsen@gmail.com>
2020-01-13 20:41:07 +0100

    :sparkles: Add game manager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+	public static GameManager instance = null;
+
+	// Add refernce to LevelManager
+
+	private bool _doingSetup = true;
+
+	void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+		else if (instance != this)
+		{
+			Destroy(gameObject);
+		}
+
+		InitGame();
+	}
+
+	void InitGame()
+	{
+		// Setup scene in level manager
+	}
+}
